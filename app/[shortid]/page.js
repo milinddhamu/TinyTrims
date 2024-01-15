c68035006c0f6e1f1e00a45c628fbf5c28543123
Milind Dhamu <milind.dhamu.111@gmail.com>
2024-01-15 11:58:26 +0530

    Issue: metadata is having some issue as its not generated as static but onDemand.

--- app/[shortid]/page.js
+++ app/[shortid]/page.js
@@ -15,25 +15,41 @@ async function getLinkData(params) {
   return await getLinkDataByShortid(shortid);
 }
 
-export async function generateMetadata({ params, searchParams }) {
-  const data = await getLinkData(params);
-  const matchedData = data?.links.find(entry => entry !== null);
+// export async function generateMetadata({ params, searchParams }) {
+//   const data = await getLinkData(params);
+//   const matchedData = data?.links.find(entry => entry !== null);
 
-  return {
-    metadataBase: new URL('https://localhost:3000'),
-    title: matchedData.title,
-    description: matchedData.description,
+//   return {
+//     metadataBase: new URL('https://localhost:3000'),
+//     title: matchedData.title,
+//     description: matchedData.description,
+//     openGraph: {
+//       title: matchedData.title,
+//       description: matchedData.description,
+//       images: [matchedData.image],
+//     },
+//     twitter: {
+//       title: matchedData.title,
+//       description: matchedData.description,
+//       images: [matchedData.image], // Must be an absolute URL
+//     },
+//   };
+// };
+
+
+export const metadata = {
+    title: "Tiny Trims",
+    description: "",
     openGraph: {
-      title: matchedData.title,
-      description: matchedData.description,
-      images: [matchedData.image],
+      title: "",
+      description: "",
+      images: [],
     },
     twitter: {
-      title: matchedData.title,
-      description: matchedData.description,
-      images: [matchedData.image], // Must be an absolute URL
+      title: "",
+      description: "",
+      images: [], 
     },
-  };
 }
 
 export default async function Page({ params }) {
@@ -41,6 +57,14 @@ export default async function Page({ params }) {
   const matchedLinkData = data?.links.find(entry => entry !== null);
   if (matchedLinkData) {
     const destination = matchedLinkData.destination;
+    metadata.title = matchedLinkData.title
+    metadata.description = matchedLinkData.description 
+    metadata.openGraph.title = matchedLinkData.title
+    metadata.openGraph.description = matchedLinkData.description
+    metadata.openGraph.images = [matchedLinkData.image]
+    metadata.twitter.title = matchedLinkData.title
+    metadata.twitter.description = matchedLinkData.description
+    metadata.twitter.images = [matchedLinkData.image]
     if (destination) {
       // console.log('redirected')
       return redirect(destination);
